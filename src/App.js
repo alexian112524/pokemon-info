a0c406e630f99e1fa09ff53d28ae3e1656a5922e
Alexian <alexian@scorrd.com>
2020-11-04 23:07:59 +0100

    Affichage des pokemons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import CardList from './CardList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+  const [next, setNext] = useState(null);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setPokemons(result.results);
+          setNext(result.next);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  function handleShowMore() {
+    if(next) {
+      fetch(next)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            console.log(result.next);
+            setPokemons(pokemons.concat(result.results));
+            setNext(result.next);
+          },
+          (error) => {
+            console.log(error.message);
+          }
+        )
+    }
+  }
+
+  if(error) {
+    return (
+      <div className="App">
+        <h1>Erreur: {error.message}</h1>
+      </div>
+    );
+  }
+  else if(!isLoaded) {
+    return (
+      <div className="App">
+        <h1>Chargement...</h1>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="App">
+        <h1>Welcome on Pokemon infos</h1>
+        <CardList pokemons={pokemons}></CardList>
+        {next ? <button onClick={handleShowMore}>Show more</button> : <span></span>}
+      </div>
+    );
+  }
+};
 
 export default App;
